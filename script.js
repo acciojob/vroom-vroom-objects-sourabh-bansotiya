4e80d6319e0cbc9b72ced6e6f4847f0cbbb29d65
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-22 20:29:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make;
+	this.model;
+}
+getMakeModel(){
+	return `${make}, ${model}`;
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Object.setPrototypeOf(SportsCar, Car);
+}
+getTopSpeed(){
+	return `${topSpeed}`;
+}
+
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+console.log(car.getTopSpeed()); // Output: 200
 
 // Do not change the code below
 window.Car = Car;
